d1d00f20d2671aff1f0adfb8784af84dbc98607a
Piotr <1brozek.peter@gmail.com>
2025-10-18 14:44:04 -0500

    work in progress - move to piotr-brozek-branch

---
+++ frontend/fitness-app/src/pages/Settings.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Settings() {
+  return (
+    <div style={{ 
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "black",
+      color: "white"
+    }}>
+      <h1>Settingssdsds</h1>
+      <p>Welcome to your settings page!</p>
+    </div>
+  );
+}
+
+export default Settings;
