b5fbc907458f8b7e7dc7057427cf2d1b21b3b91a
Piotr <1brozek.peter@gmail.com>
2025-10-19 18:57:24 -0500

    fixed settings missing

---
+++ frontend/fitness-app/src/pages/Settings.jsx
@@ -0,0 +1,14 @@
+// Settings.js
+import React from "react";
+
+
+function SettingsContainer() {
+  return (
+    <div className="Settings" style={{ display: "flex", height: "100vh" }}>
+      
+      <h1>settings</h1>
+    </div>
+  );
+}
+
+export default SettingsContainer;
